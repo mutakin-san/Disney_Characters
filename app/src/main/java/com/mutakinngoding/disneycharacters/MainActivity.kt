91db3185f6fafac80f5ba9e03a6090deacbe8b25
mutakin <mutakin.email@gmail.com>
2022-09-20 12:51:17 +0700

    Remove comment

--- app/src/main/java/com/mutakinngoding/disneycharacters/MainActivity.kt
+++ app/src/main/java/com/mutakinngoding/disneycharacters/MainActivity.kt
@@ -25,8 +25,7 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         val navController = navHostFragment.navController
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_favorite
