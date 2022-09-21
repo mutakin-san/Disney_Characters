6c3411fd87d438e3f70524249cb60da18951faf6
mutakin <mutakin.email@gmail.com>
2022-09-21 11:22:28 +0700

    Change layout and action bar theme

--- app/src/main/java/com/mutakinngoding/disneycharacters/MainActivity.kt
+++ app/src/main/java/com/mutakinngoding/disneycharacters/MainActivity.kt
@@ -21,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+
         val navView: BottomNavigationView = binding.navView
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
