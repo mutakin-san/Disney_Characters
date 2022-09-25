9a7e7c7ae9eb41ee2f70c226e9ce4f2099d25579
mutakin <mutakin.email@gmail.com>
2022-09-25 19:47:28 +0700

    Add useCase test file

---
+++ core/src/test/java/com/mutakinngoding/disneycharacters/core/domain/usecase/CharacterUseCaseTest.kt
@@ -0,0 +1,20 @@
+package com.mutakinngoding.disneycharacters.core.domain.usecase
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class CharacterUseCaseTest {
+
+    @Test
+    fun getAllCharacter() {
+    }
+
+    @Test
+    fun getFavoriteCharacter() {
+    }
+
+    @Test
+    fun setFavoriteCharacter() {
+    }
+}
