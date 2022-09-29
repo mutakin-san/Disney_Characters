f27ee075c0527fb80458e49215da6ff4cd4ae29d
mutakin <mutakin.email@gmail.com>
2022-09-29 10:18:33 +0700

    Add Encryption to sqlite database, and activate pro guard minified code

--- core/src/main/java/com/mutakinngoding/disneycharacters/core/di/DatabaseModule.kt
+++ core/src/main/java/com/mutakinngoding/disneycharacters/core/di/DatabaseModule.kt
@@ -2,10 +2,13 @@ package com.mutakinngoding.disneycharacters.core.di
 
 import android.content.Context
 import androidx.room.Room
+import com.mutakinngoding.disneycharacters.core.R
 import com.mutakinngoding.disneycharacters.core.data.source.local.room.DisneyCharactersDao
 import com.mutakinngoding.disneycharacters.core.data.source.local.room.DisneyDatabase
 import dagger.Module
 import dagger.Provides
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -14,11 +17,16 @@ class DatabaseModule {
     @Singleton
     @Provides
     fun provideDatabase(context: Context) : DisneyDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes(context.getString(R.string.sqlite_encrypt_key).toCharArray())
+        val factory = SupportFactory(passphrase)
         return Room.databaseBuilder(
             context.applicationContext,
             DisneyDatabase::class.java,
             "disney.db"
-        ).fallbackToDestructiveMigration().build()
+        )
+            .openHelperFactory(factory)
+            .fallbackToDestructiveMigration()
+            .build()
     }
 
 
