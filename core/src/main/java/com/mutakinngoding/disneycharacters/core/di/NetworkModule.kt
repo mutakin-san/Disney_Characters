da5246b7f327c787800b08c1a1189b19406f429f
mutakin <mutakin.email@gmail.com>
2022-09-29 21:04:12 +0700

    add certificate pinning

--- core/src/main/java/com/mutakinngoding/disneycharacters/core/di/NetworkModule.kt
+++ core/src/main/java/com/mutakinngoding/disneycharacters/core/di/NetworkModule.kt
@@ -4,6 +4,7 @@ import com.mutakinngoding.disneycharacters.core.BuildConfig
 import com.mutakinngoding.disneycharacters.core.data.source.remote.network.DisneyCharactersService
 import dagger.Module
 import dagger.Provides
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -14,6 +15,12 @@ import java.util.concurrent.TimeUnit
 class NetworkModule {
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val hostname = BASE_URL.removePrefix("https://").removeSuffix("/")
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/r6NKPGIgMidjJuLcWUyWnCPeGEU5feUJmUdxBpDPpVY=")
+            .add(hostname, "sha256/jQJTbIh0grw0/1TkHSumWb+Fs0Ggogr621gT3PvPKG0=")
+            .add(hostname, "sha256/C5+lpZ7tcVwmwQIMcRtPbsQtWLABXhQzejna0wHFr8M=")
+            .build()
         val loggingInterceptor =
             HttpLoggingInterceptor()
         if (BuildConfig.DEBUG) {
@@ -24,6 +31,7 @@ class NetworkModule {
         return OkHttpClient.Builder().addInterceptor(loggingInterceptor)
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
@@ -31,11 +39,15 @@ class NetworkModule {
     @Provides
     fun provideApiService(client: OkHttpClient): DisneyCharactersService {
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.disneyapi.dev/")
+            .baseUrl(BASE_URL)
             .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         return retrofit.create(DisneyCharactersService::class.java)
+    }
+
+    companion object {
+        private const val BASE_URL = "https://api.disneyapi.dev/"
     }
 }
